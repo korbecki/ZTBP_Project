005d0f38092ef05b41845c9afdc4ac2ccc66f925
Krystian Korbecki <krystian.korbecki366@gmail.com>
2024-10-20 15:09:13 +0200

    Init

---
+++ src/main/java/pl/ztbd/project/ZtbpProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.ztbd.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtbpProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZtbpProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/ztbd/project/ZtbpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ztbd.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZtbpProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
